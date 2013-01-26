63171639468566201757e973d51188f7391e0aa4
Newbilius <newbilius@gmail.com>
2013-01-26 12:40:58 +0600

    Поправил настройки аддона экспорта в яндекс.маркет.

--- site/addons/yandex_export/config.php
+++ site/addons/yandex_export/config.php
@@ -3,12 +3,13 @@ global $_top_menu;
 $_top_menu[] = Array(
     "caption" => "-",
     "parent" => "utilits",
+	"admin_right" => true,
 );
 
 $_top_menu[] = Array(
     "url" => "/site/run/yandex_export.php",
     "caption" => "Ручной запуск экспорта в яндекс.маркет файл",
-    "admin_right" => false,
+    "admin_right" => true,
     "parent" => "utilits",
 );
 ?>
